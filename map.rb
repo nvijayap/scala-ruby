5fcdcd77c981f0e516fd62b9c1158ebcb9475011
Naga Vijayapuram <naga_vijayapuram@gap.com>
2014-12-30 20:59:23 -0800

    map

---
+++ map.rb
@@ -0,0 +1,7 @@
+array = [1,2,3]
+puts "\n=> array: \t\t " + array.to_s
+
+mapped = array.map {|e| e*2}
+puts "\n=> mapped array: \t " + mapped.to_s
+
+puts
---
+++ map.scala
@@ -0,0 +1,7 @@
+val list = List(1,2,3)
+println("\n=> list: \t\t " + list)
+
+val mapped = list map (_ * 2)
+println("\n=> mapped list: \t " + mapped)
+
+println
